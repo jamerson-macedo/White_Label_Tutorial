628d9be45d3f809a5b33b51129c8f0a089828da7
jamerson Macedo <jamerson_macedo@hotmail.com>
2023-12-27 18:57:46 -0300

    implementando banco de dados

--- app/src/main/java/br/com/douglasmotta/whitelabeltutorial/data/FirebaseProductDataSource.kt
+++ app/src/main/java/br/com/douglasmotta/whitelabeltutorial/data/FirebaseProductDataSource.kt
@@ -5,8 +5,10 @@ import br.com.douglasmotta.whitelabeltutorial.BuildConfig
 import br.com.douglasmotta.whitelabeltutorial.domain.model.Product
 import br.com.douglasmotta.whitelabeltutorial.util.COLLECTION_PRODUCTS
 import br.com.douglasmotta.whitelabeltutorial.util.COLLECTION_ROOT
+import br.com.douglasmotta.whitelabeltutorial.util.STORAGE_IMAGES
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
+import java.util.UUID
 import kotlin.coroutines.suspendCoroutine
 
 class FirebaseProductDataSource(
@@ -40,10 +42,35 @@ class FirebaseProductDataSource(
     }
 
     override suspend fun uploadProductImage(img: Uri): String {
-        TODO("Not yet implemented")
+        return suspendCoroutine { continuation ->
+            val randomKey = UUID.randomUUID()
+            // images/car/id
+            val childreference =
+                storageRef.child("$STORAGE_IMAGES/${BuildConfig.FIREBASE_FLAVOR_COLLECTION}/$randomKey")
+            childreference.putFile(img).addOnSuccessListener { taskSnapshot ->
+                // pego a url da foto
+                taskSnapshot.storage.downloadUrl.addOnSuccessListener { uri ->
+                    val patch = uri.toString()
+                    continuation.resumeWith(Result.success(patch))
+                    // mando a foto
+                }
+
+            }.addOnFailureListener { exception ->
+                continuation.resumeWith(Result.failure(exception))
+
+            }
+        }
     }
 
     override suspend fun createProduct(product: Product): Product {
-        TODO("Not yet implemented")
+        return suspendCoroutine { continuation ->
+            docRef.collection(COLLECTION_PRODUCTS).document(System.currentTimeMillis().toString())
+                .set(product).addOnSuccessListener {
+                    continuation.resumeWith(Result.success(product))
+
+                }.addOnFailureListener {
+                    continuation.resumeWith(Result.failure(it))
+                }
+        }
     }
 }
